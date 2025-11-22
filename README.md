79600971346c21550b4c9377bf29e2020a93fd6d
Raven Du <raven.du@hotmail.com>
2025-11-22 23:49:47 +0800

    add files.

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 自建 git-lfs 服务器维护相关工具。
 
+## for windows 
+
+``` cmd
+go install github.com/git-lfs/lfs-test-server@latest
+.\lfs-test-server.cmd --admin
+```
+
 `unix2dos.exe .gitignore`
+
+## docker
--- lfs-test-server.cmd
+++ lfs-test-server.cmd
@@ -2,8 +2,8 @@
 setlocal enabledelayedexpansion
 
 :: 从配置文件中读取配置
-set CONFIG_FILE=config.conf
-for /f "tokens=1,2 delims==" %%A in ('findstr /r "LFS_LISTEN LFS_HOST LFS_METADB LFS_CONTENTPATH LFS_ADMINUSER LFS_ADMINPASS LFS_CERT LFS_KEY LFS_SCHEME LFS_USETUS LFS_TUSHOST" "%CONFIG_FILE%"') do (
+set CONFIG_FILE=lfs-test-server.conf
+for /f "tokens=1,2 delims==" %%A in ('findstr /r "LFS_LISTEN LFS_HOST LFS_METADB LFS_CONTENTPATH LFS_ADMINUSER LFS_ADMINPASS LFS_CERT LFS_KEY LFS_SCHEME LFS_USETUS LFS_TUSHOST LFS_PUBLIC" "%CONFIG_FILE%"') do (
     set "key=%%~A"
     set "value=%%~B"
     set "!key!=!value!"
